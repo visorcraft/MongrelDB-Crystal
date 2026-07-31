fd36ba888f85ac3f7edecfb910b321ab53cb9da0
visorcraft <thomas@visorcraft.com>
2026-07-31 11:27:00 -0500

    release 0.64.15
    Align package version and MongrelDB / MongrelDB Kit dependencies with
    engine and kit 0.64.15.

--- src/mongreldb.cr
+++ src/mongreldb.cr
@@ -24,7 +24,7 @@ require "base64"
 #
 # See https://www.MongrelDB.com for the daemon and full documentation.
 module MongrelDB
-  VERSION = "0.64.14"
+  VERSION = "0.64.15"
 
   # Default daemon address used when none is supplied.
   DEFAULT_BASE_URL = "http://127.0.0.1:8453"
